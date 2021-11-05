03002fd33021e32673b00826dbe01a0f5473af8e
Kerem <90256478+keremmor@users.noreply.github.com>
2021-11-05 11:18:38 +0300

    Update WeeklyConsumption.py

--- WeeklyConsumption.py
+++ WeeklyConsumption.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv (r"C:\Users\KEREM\Downloads\archive\daily_dataset\daily_dataset\block_0.csv",nrows=7)
-df = pd.DataFrame(data, columns=["day", "energy_sum"])
-df.plot(x="day", y=[ "energy_sum"], kind="bar", figsize=(8,9))
-plt.ylabel("Energy Consumption (KiloWatt*Hour)")
-plt.xlabel("Days")
-plt.title("Weekly Consumption of a HouseHold")
-plt.show()
+
+def weekly_consumption_of_household(path, start_date, end_date):
+    data = pd.read_csv(path)
+    df = pd.DataFrame(data, columns=["day", "daily_energy_sum"])
+    mask = (df['day'] > start_date) & (df['day'] <= end_date)
+    df = df.loc[mask]
+    df.plot(x="day", y=["daily_energy_sum"], kind="line", figsize=(8, 9))
+    plt.ylabel("Energy Consumption (KiloWatt*Hour)")
+    plt.xlabel("Days")
+    plt.title("Weekly Consumption of a HouseHold")
+    plt.show()
+
+
+path = "new_datas/output_tables/block_0_mixed.csv"
+start_date = '2013-01-12'
+end_date = '2013-01-19'
+weekly_consumption_of_household(path, start_date, end_date)
